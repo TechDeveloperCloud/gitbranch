4694954187e395a42ddfb16fb8dda7e343281a40
DeveloperTech <160417509+TechDeveloperCloud@users.noreply.github.com>
2026-07-18 10:18:22 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,20 @@
+public class BankAccount {
+
+    private double balance;
+
+    // Deposit method
+    public void deposit(double amount) {
+
+        if (amount > 0) {
+            balance = balance + amount;
+            System.out.println("Amount deposited successfully.");
+            System.out.println("Current Balance: " + balance);
+        } else {
+            System.out.println("Deposit amount must be greater than zero.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
